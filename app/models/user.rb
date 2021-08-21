93fa00477b44c4b2865725ff9243b62b98b91cfb
matthew-deluge <matthewj913@gmail.com>
2021-08-21 17:43:15 -0400

    Added user and posts models with associations

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true, length: { in: 7..20 }
+  validates :email, presence: true
+  validates :password, presence: true, length: { in: 6..12 }
+  has_many :posts
+end
